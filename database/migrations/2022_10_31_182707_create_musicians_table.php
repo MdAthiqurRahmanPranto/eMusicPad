d2903d19e0f964db55de0a7d73f2bbf343818489
pranto <prantokhan1996@gmail.com>
2022-11-01 01:13:38 +0600

    new database

--- database/migrations/2022_10_31_182707_create_musicians_table.php
+++ database/migrations/2022_10_31_182707_create_musicians_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('musicians', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('band')->nullable();
             $table->timestamps();
         });
     }
